34bd39c59b705a1e81acc9295454d5a15c8cd44b
pinto0309 <rmsdh122@yahoo.co.jp>
2026-03-25 19:34:47 +0900

    single gpu

--- train.sh
+++ train.sh
@@ -23,8 +23,6 @@ export MIN_PIXELS=3136
 export MAX_PIXELS=100352
 export USE_HF=1
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True  # Reduce memory fragmentation
-export NCCL_IB_DISABLE=1
-export NCCL_SOCKET_IFNAME=ens6
 export STREAM_FRAME_CACHE_DIR=/mnt/data/downloads/stream/frames
 export TORCH_DISTRIBUTED_DEBUG=DETAIL
 
@@ -81,7 +79,7 @@ uv run swift sft \
 --logging_steps 5 \
 --output_dir output \
 --warmup_ratio 0.05 \
---deepspeed zero3 \
+# --deepspeed zero3 \
 --use_liger_kernel true \
 --max_length 32768 \
 --dataset_num_proc 4 \
