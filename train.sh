227e3709d90cb0c725f07266e7dc28e94b4b9f08
pinto0309 <rmsdh122@yahoo.co.jp>
2026-03-25 19:31:17 +0900

    mpi4py

--- train.sh
+++ train.sh
@@ -23,6 +23,8 @@ export MIN_PIXELS=3136
 export MAX_PIXELS=100352
 export USE_HF=1
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True  # Reduce memory fragmentation
+export NCCL_IB_DISABLE=1
+export NCCL_SOCKET_IFNAME=ens6
 export STREAM_FRAME_CACHE_DIR=/mnt/data/downloads/stream/frames
 export TORCH_DISTRIBUTED_DEBUG=DETAIL
 
